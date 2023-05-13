cb0cd8b868379e0def9eb70b324ded15e01abcf0
GimGirin <theodoreyjg@gmail.com>
2023-05-12 17:12:53 -0700

    Initial commit

---
+++ src/main/java/net/gim/bcfoundry/BetterCombatFoundry.java
@@ -0,0 +1,15 @@
+package net.gim.bcfoundry;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BetterCombatFoundry implements ModInitializer {
+	public static final String MOD_ID = "bcfoundry";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
